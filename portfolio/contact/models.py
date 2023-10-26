2db9e14629bfdd0ed16980ff8daf72640ce43127
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-26 12:01:01 -0300

    feat(contact model)

--- portfolio/contact/models.py
+++ portfolio/contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    linkedin_url = models.URLField(blank=True, null=True)
+    address = models.CharField(blank=True, null=True, max_length=50)
