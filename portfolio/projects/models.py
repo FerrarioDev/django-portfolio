28ddb3ce34715a9bbb297b4568fcf85477c2e66a
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-26 11:51:34 -0300

    feat(project model)

--- portfolio/projects/models.py
+++ portfolio/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    technologies = models.CharField( max_length=255)
+    Project_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField()
+    image = models.ImageField(upload_to='project_images/')
+    completion_date = models.DateField()
+    achievements = models.TextField(blank=True, null=True)
