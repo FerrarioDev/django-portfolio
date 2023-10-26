b4b0af4ade40139b02e9f05acc96e494e43b6215
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-26 11:57:32 -0300

    feat(skills model)

--- portfolio/skills/models.py
+++ portfolio/skills/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Skill(models.Model):
     name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
-    experience_level = models.CharField( 
-        max_length=50
+    experience_level = models.CharField(
+        max_length=50,
         choices=[('Beginner', 'beginner'), ('Intermediate', 'intermediate'), ('Expert', 'Expert')]
     )
