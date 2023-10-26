11613e2ac470f6ff66e5e8bc492cd33720ea9ec1
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-26 12:43:52 -0300

    test(helloworld frontend)

--- portfolio/projects/tests.py
+++ portfolio/projects/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectModelTestCase(TestCase):
+    def test_project_creation(self):
+        project = Project.objects.create(
+            title="Test Project",
+            description="This is a test project",
+            technologies="Django, React",
+            github_url="https://github.com/test/project",
+            completion_date="2023-01-01",
+        )
+        self.assertEqual(project.title, "Test Project")
+        # Add more test assertions as needed
